5436c9301847db7d8531017c1ee134f26ca02535
Christian <christian.mig.flmo@gmail.com>
2025-06-21 12:48:10 -0600

    Adding basic validations for inputs of Bill and NumOfPeople

--- scripts/script.js
+++ scripts/script.js
@@ -29,4 +29,61 @@ function tipSelectionListener(e) {
 	tipPercentage = e.currentTarget.value;
 }
 
-/* Form validations */
+/* ======================== Form validations =========================== */
+/*
+*  input.checkValidity()
+*  input.validity.valid
+*
+* */
+const inputPeopleEl = document.querySelector('.app__peopleInput');
+const inputBillEl = document.querySelector('.app__billInput');
+
+inputPeopleEl.addEventListener('input', validationListener);
+inputBillEl.addEventListener('input', validationListener);
+
+function validationListener(e) {
+	const currentInput = e.target;
+
+	if (!currentInput.validity.valid) {
+		setErrorMessage(currentInput, "Invalid input characters");
+		return;
+	}
+
+	if (currentInput.value && Number(currentInput.value) === 0) {
+		setErrorMessage(currentInput, "Value can't be zero nor empty");
+		return;
+	}
+
+	if (currentInput.classList.contains('app__peopleInput') && Number(currentInput.value) > 100) {
+		setErrorMessage(currentInput, "Value out of range (Max. 100 people)");
+		return;
+	}
+
+	clearErrorMessage(currentInput);
+}
+
+function setErrorMessage(element, message) {
+	element.classList.add('invalid');
+	const parentBox = element.closest('.inputBox');
+
+	if(!parentBox) return;
+
+	const errorMsgEl = parentBox.querySelector('.app__error-message');
+
+	if (errorMsgEl.classList.contains('hidden')) {
+		errorMsgEl.textContent = message;
+		errorMsgEl.classList.remove('hidden');
+	}
+}
+
+function clearErrorMessage(element) {
+	element.classList.remove('invalid');
+	const parentBox = element.closest('.inputBox');
+
+	if(!parentBox) return;
+
+	const errorMsgEl = parentBox.querySelector('.app__error-message');
+	if (!errorMsgEl.classList.contains('hidden'))
+		errorMsgEl.classList.add('hidden');
+}
+
